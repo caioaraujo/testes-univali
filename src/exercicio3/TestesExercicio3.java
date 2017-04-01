55f2c1e06fd8ac1d1e25731470cf2b7b453cfef2
Caio Felipe de Araujo <c.felipe.araujo@gmail.com>
2017-04-01 11:17:26 -0300

    Exercicio da hora

--- src/exercicio3/TestesExercicio3.java
+++ src/exercicio3/TestesExercicio3.java
@@ -3,6 +3,7 @@ package exercicio3;
 import static org.junit.Assert.*;
 
 import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 import org.junit.Test;
 
 public class TestesExercicio3 {
@@ -17,10 +18,17 @@ public class TestesExercicio3 {
 	
 	@Test
 	public void testMinhaDataDeNascimento() {
-		LocalDate myBirthdate = new LocalDate(1986, 10, 6); // Teste hipotetico. Eu nao nasci em 86 :x
+		LocalDate myBirthdate = new LocalDate(1986, 10, 6); // teste hipotetico. Eu nao nasci em 86
 		assertEquals(6, myBirthdate.getDayOfMonth());
 		assertEquals(10, myBirthdate.getMonthOfYear());
 		assertEquals(1986, myBirthdate.getYear()); 
 	}
-
+	
+	@Test
+	public void testHorarioQueAcordeiHoje() {
+		LocalTime wokeUpTime = new LocalTime(11, 59); // hoje eh sabado
+		assertEquals(11, wokeUpTime.getHourOfDay());
+		assertEquals(59, wokeUpTime.getMinuteOfHour());
+	}
+	
 }
