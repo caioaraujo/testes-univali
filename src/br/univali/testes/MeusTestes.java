c7b92e8fd66968b30fe851562b3aa6b718cc8cde
Caio Felipe de Araujo <6237568@univali.br>
2017-04-01 10:21:44 -0300

    first commit

---
+++ src/br/univali/testes/MeusTestes.java
@@ -0,0 +1,20 @@
+package br.univali.testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MeusTestes {
+	
+	@Test
+	public void meuTeste() throws Exception {
+		String nome = "aaa";
+		assertEquals("aaa", nome);
+	}
+	
+	@Test(expected=ArithmeticException.class)
+	public void meuTeste2() throws Exception {
+		double aaa = 12 / 0;
+	}
+
+}
