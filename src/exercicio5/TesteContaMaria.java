190029a5631d7b58df13cf117ef14d3308f1bc59
6237568 <6237568@U707906.univali.br>
2017-04-08 09:59:25 -0300

    Testes que faltavam

--- src/exercicio5/TesteContaMaria.java
+++ src/exercicio5/TesteContaMaria.java
@@ -1,12 +1,16 @@
 package exercicio5;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import br.ufsc.ine.leb.sistemaBancario.Agencia;
 import br.ufsc.ine.leb.sistemaBancario.Conta;
+import br.ufsc.ine.leb.sistemaBancario.Dinheiro;
+import br.ufsc.ine.leb.sistemaBancario.Entrada;
+import br.ufsc.ine.leb.sistemaBancario.Saida;
+import br.ufsc.ine.leb.sistemaBancario.Transacao;
 
 public class TesteContaMaria {
 	
@@ -19,12 +23,66 @@ public class TesteContaMaria {
 
 	@Test
 	public void testContaMaria() {
-		String nomeMaria = "Maria";
-		Conta contaMaria = agenciaCentro.criarConta(nomeMaria);
+		Conta contaMaria = createContaMaria(agenciaCentro);
 		assertEquals("0001-5", contaMaria.obterIdentificador());
-		assertEquals(nomeMaria, contaMaria.obterTitular());
+		assertEquals("Maria", contaMaria.obterTitular());
 		assertEquals("0,00", contaMaria.calcularSaldo().obterQuantia().formatado());
 		assertEquals("Centro", contaMaria.obterAgencia().obterNome());
+	}
+	
+	@Test
+	public void testDepositoDezConto() {
+		Conta contaMaria = createContaMaria(agenciaCentro);
+		// Deposito de 10 reais
+		Dinheiro quantia = new Dinheiro(agenciaCentro.obterBanco().obterMoeda(), 10, 0);
+		this.realizarDeposito(contaMaria, quantia);
+		
+		assertEquals(quantia, contaMaria.calcularSaldo().obterQuantia());
+	}
+	
+	@Test
+	public void testSaqueSeisConto() {
+		Conta contaMaria = createContaMaria(agenciaCentro);
+		// Deposito de 10 reais
+		Dinheiro quantia = new Dinheiro(agenciaCentro.obterBanco().obterMoeda(), 10, 0);
+		this.realizarDeposito(contaMaria, quantia);
+		// Saque de 6 reais
+		quantia = new Dinheiro(agenciaCentro.obterBanco().obterMoeda(), 6, 0);
+		this.realizarSaque(contaMaria, quantia);
+		
+		assertEquals("+4,00 BRL", contaMaria.calcularSaldo().formatado());
+	}
+	
+	@Test 
+	public void testSaqueInsuficiente() {
+		Conta contaMaria = createContaMaria(agenciaCentro);
+		// Deposito de 10 reais
+		Dinheiro quantia = new Dinheiro(agenciaCentro.obterBanco().obterMoeda(), 10, 0);
+		this.realizarDeposito(contaMaria, quantia);
+		// Saque de 6 reais
+		quantia = new Dinheiro(agenciaCentro.obterBanco().obterMoeda(), 6, 0);
+		this.realizarSaque(contaMaria, quantia);
+		
+		// Tentativa de saque de 8 reais
+		quantia = new Dinheiro(agenciaCentro.obterBanco().obterMoeda(), 8, 0);
+		this.realizarSaque(contaMaria, quantia);
+		
+		assertEquals("+4,00 BRL", contaMaria.calcularSaldo().formatado());
+	}
+	
+	private Conta createContaMaria(Agencia agencia) {
+		Conta contaMaria = agencia.criarConta("Maria");
+		return contaMaria;
+	}
+	
+	private void realizarDeposito(Conta conta, Dinheiro quantia) {
+		Transacao transacao = new Entrada(conta, quantia);
+		conta.adicionarTransacao(transacao);
+	}
+	
+	private void realizarSaque(Conta conta, Dinheiro quantia) {
+		Transacao transacao = new Saida(conta, quantia);
+		conta.adicionarTransacao(transacao);
 	}
 
 }
